e478c169db9cb81c4c83c2fdf4236b75b748222e
0112shpark <000112shpark@naver.com>
2023-04-10 14:32:42 +0900

    :lipstick:style change

--- src/App.js
+++ src/App.js
@@ -1,106 +1,248 @@
 import "./App.css";
 
 function App() {
-  const handleChange = (e) => {
-    //username at signin page
-    console.log(e.target.value);
-  };
+  window.addEventListener("load", start);
+
+  function start() {
+    /* ===========================
+    Elements Selectors
+============================ */
+
+    const elm = {
+      arrow: document.querySelector(".form-container__arrow"),
+      overlay: document.querySelector(".overlay"),
+      title: document.querySelector(".title"),
+      signUpButton: document.querySelector(".buttons__signup"),
+      loginButton: document.querySelector(".buttons__signup--login"),
+      loginForm: document.querySelector(".login-form"),
+      registerForm: document.querySelector(".login-form--register"),
+    };
+
+    /* ===========================
+    Properties Object
+============================ */
+
+    const props = {
+      left: "left: 20px;",
+      bottom: "bottom: -500px;",
+      transition1: "transition: bottom 1s;",
+      transition2: "transition: bottom 2s;",
+      opacity0: "opacity: 0;",
+      opacity1: "opacity: 1;",
+      trnsDelay: "transition-delay: 1s;",
+      zIndex: "z-index: 6;",
+      left0: "left: 0;",
+      trnsDelay0: "transition-delay: 0s;",
+      zIndex0: "z-index: 0;",
+      leftM120: "left: -120px;",
+    };
+
+    /* ===========================
+    Elements Array
+============================ */
+
+    const elms = [
+      elm.arrow,
+      elm.overlay,
+      elm.title,
+      elm.signUpButton,
+      elm.loginButton,
+      elm.loginForm,
+      elm.registerForm,
+    ];
+
+    function transition(elements, props) {
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].setAttribute("style", `${props[i]}`);
+      }
+    }
+
+    /* ===========================
+    Events
+============================ */
+
+    document.getElementById("signUp").onclick = function () {
+      const properties = [
+        props.left,
+        props.opacity0,
+        props.opacity0,
+        `${props.transition1} ${props.bottom}`,
+        `${props.transition2} ${props.bottom}`,
+        props.opacity0,
+        `${props.opacity1} ${props.trnsDelay} ${props.zIndex}`,
+      ];
+
+      transition(elms, properties);
+    };
+
+    document.getElementById("login").onclick = function () {
+      const properties = [
+        props.left,
+        props.opacity0,
+        props.opacity0,
+        `${props.transition1} ${props.bottom}`,
+        `${props.transition2} ${props.bottom}`,
+        `${props.opacity1} ${props.trnsDelay} ${props.zIndex}`,
+        props.opacity0,
+      ];
+
+      transition(elms, properties);
+    };
+
+    document.getElementById("arrowClick").onclick = function () {
+      const properties = [
+        props.leftM120,
+        props.opacity1,
+        props.opacity1,
+        props.opacity1,
+        props.opacity1,
+        `${props.opacity0} ${props.trnsDelay0} ${props.zIndex0}`,
+        `${props.opacity0} ${props.trnsDelay0} ${props.zIndex0}`,
+      ];
+
+      transition(elms, properties);
+    };
+  }
 
   return (
-    <div className="login-wrap">
-      <div className="login-html">
-        <input id="tab-1" type="radio" name="tab" className="sign-in" />
-        <label htmlFor="tab-1" className="tab">
-          Sign In
-        </label>
-        <input id="tab-2" type="radio" name="tab" className="sign-up" />
-        <label htmlFor="tab-2" className="tab">
-          Sign Up
-        </label>
-        <div className="login-form">
-          <div className="sign-in-htm">
-            <div className="group">
-              <label htmlFor="user" className="label">
-                Username
-              </label>
-              <input
-                id="user1"
-                type="text"
-                className="input"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="group">
-              <label htmlFor="pass" className="label">
-                Password
-              </label>
-              <input
-                id="pass1"
-                type="password"
-                className="input"
-                data-type="password"
-              />
-            </div>
-            <div className="group">
-              <input id="check" type="checkbox" className="check" />
-              <label htmlFor="check">
-                <span className="icon"></span> Keep me Signed in
-              </label>
-            </div>
-            <div className="group">
-              <input type="submit" className="button" value="Sign In" />
-            </div>
-            <div className="hr"></div>
-            <div className="foot-lnk">
-              <a href="#htmlForgot">htmlForgot Password?</a>
-            </div>
-          </div>
-          <div className="sign-up-htm">
-            <div className="group">
-              <label htmlFor="user" className="label">
-                Username
-              </label>
-              <input id="user" type="text" className="input" />
-            </div>
-            <div className="group">
-              <label htmlFor="pass" className="label">
-                Password
-              </label>
-              <input
-                id="pass"
-                type="password"
-                className="input"
-                data-type="password"
-              />
-            </div>
-            <div className="group">
-              <label htmlFor="pass" className="label">
-                Repeat Password
-              </label>
-              <input
-                id="pass2"
-                type="password"
-                className="input"
-                data-type="password"
-              />
-            </div>
-            <div className="group">
-              <label htmlFor="pass" className="label">
-                Email Address
-              </label>
-              <input id="pass3" type="text" className="input" />
-            </div>
-            <div className="group">
-              <input type="submit" className="button" value="Sign Up" />
-            </div>
-            <div className="hr"></div>
-            <div className="foot-lnk">
-              <label htmlFor="tab-1">Already Member?</label>
-            </div>
+    <section className="form-container">
+      <span id="arrowClick" className="form-container__arrow">
+        <i className="fa fa-arrow-circle-left" aria-hidden="true"></i>
+      </span>
+      <div className="overlay"></div>
+      <div className="choose-form">
+        <div className="title">
+          <h1 className="title__h1">
+            Welcome
+            <br />
+            Start for free
+          </h1>
+        </div>
+        <div className="buttons">
+          <a id="signUp" className="buttons__signup" href="#">
+            Sign up
+          </a>
+          <a
+            id="login"
+            className="buttons__signup buttons__signup--login"
+            href="#"
+          >
+            Login
+          </a>
+        </div>
+      </div>
+
+      <div className="login-form">
+        <div className="form-wrapper">
+          <form method="post">
+            <label className="form-wrapper__label" htmlFor="login-username">
+              Username
+            </label>
+            <input
+              id="login-username"
+              className="form-wrapper__input"
+              type="text"
+              placeholder="Username"
+              name="username"
+              required
+            />
+            <label className="form-wrapper__label" htmlFor="login-password">
+              Password
+            </label>
+            <input
+              id="login-password"
+              className="form-wrapper__input"
+              type="password"
+              placeholder="Password"
+              name="password"
+              pattern=".{3,}"
+              title="Password must contain at least 3 characters"
+              required
+            />
+            <a className="login-form__forgot-password" href="#">
+              Forgot password?
+            </a>
+            <button
+              className="buttons__signup buttons__signup--login-form"
+              type="submit"
+              href="#"
+            >
+              Login
+            </button>
+          </form>
+
+          <div className="social-media">
+            <h6 className="title__h2">Or connect with</h6>
+            <a className="buttons__signup buttons__signup--social" href="#">
+              <i className="fab fa-facebook-f" aria-hidden="true"></i>
+              &nbsp;facebook
+            </a>
+            <a className="buttons__signup buttons__signup--social" href="#">
+              <i className="fab fa-google" aria-hidden="true"></i>&nbsp;google
+            </a>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="login-form login-form--register">
+        <div className="form-wrapper">
+          <form method="post">
+            <label className="form-wrapper__label" htmlFor="signup-email">
+              Email
+            </label>
+            <input
+              id="signup-email"
+              className="form-wrapper__input"
+              type="email"
+              placeholder="example@hotmail.com"
+              name="email"
+              required
+            />
+            <label className="form-wrapper__label" htmlFor="signup-username">
+              Username
+            </label>
+            <input
+              id="signup-username"
+              className="form-wrapper__input"
+              type="text"
+              placeholder="Username"
+              name="username"
+              required
+            />
+            <label className="form-wrapper__label" htmlFor="signup-password">
+              Password
+            </label>
+            <input
+              id="signup-password"
+              className="form-wrapper__input"
+              type="password"
+              placeholder="Password"
+              name="password"
+              pattern=".{3,}"
+              title="Password must contain at least 3 characters"
+              required
+            />
+            <button
+              className="buttons__signup buttons__signup--sign-up-form"
+              type="submit"
+            >
+              Sign up
+            </button>
+          </form>
+
+          <div className="social-media">
+            <h2 className="title__h2">Or connect with</h2>
+            <a className="buttons__signup buttons__signup--social" href="#">
+              <i className="fab fa-facebook-f" aria-hidden="true"></i>
+              &nbsp;facebook
+            </a>
+            <a className="buttons__signup buttons__signup--social" href="#">
+              <i className="fab fa-google" aria-hidden="true"></i>&nbsp;google
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
