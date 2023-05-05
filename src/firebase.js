29ac266535527e4c11daad37e9f3782249d7ba44
0112shpark <000112shpark@naver.com>
2023-05-05 22:01:09 +0900

    :wrench: change firebase account

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAN5Yy32-z2V11Rt8Csu-RdhPVbc16WkFs",
-  authDomain: "capston-design-1.firebaseapp.com",
-  projectId: "capston-design-1",
-  storageBucket: "capston-design-1.appspot.com",
-  messagingSenderId: "610152427611",
-  appId: "1:610152427611:web:dc73bc4ba6d0d81a30cf7e",
+  apiKey: "AIzaSyAHcapNthGtxiWfwLqFJ-lAaixYIHNhDdw",
+  authDomain: "jaban-7c8c2.firebaseapp.com",
+  databaseURL: "https://jaban-7c8c2-default-rtdb.firebaseio.com",
+  projectId: "jaban-7c8c2",
+  storageBucket: "jaban-7c8c2.appspot.com",
+  messagingSenderId: "862801813260",
+  appId: "1:862801813260:web:4a8e0911e28ed43cd5b329",
+  measurementId: "G-B6Q439LLL4",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// export default analytics;
 export default app;
