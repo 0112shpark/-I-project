05a0edbd86d5ecd60f21f2b4591c599f9e511704
0112shpark <000112shpark@naver.com>
2023-05-08 15:33:08 +0900

    :zap: console.log name and email

---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="clone-menu">
+      <ul>
+        <li>
+          <a href="javascript:void(0)">스토어</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">Mac</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">iPad</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">iPhone</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">Watch</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">Airpods</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">Apple 독점 제공</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">액세서리</a>
+        </li>
+        <li>
+          <a href="javascript:void(0)">고객지원</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
