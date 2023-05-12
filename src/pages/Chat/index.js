fa55bcdd54695373d4508ec7afd9051ed144d6ad
0112shpark <000112shpark@naver.com>
2023-05-12 16:35:47 +0900

    :zap: implement chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -2,12 +2,21 @@ import React, { useEffect, useState } from "react";
 import "./main.css";
 import { useData } from "../../hooks/userData";
 import { useNavigate } from "react-router";
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  organization: "org-JbU7cwJQlyRC9PjUIczsSIaO",
+  apiKey: "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",
+});
+const my_apikey = "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";
+//const openai = new OpenAIApi(configuration);
+
 const Chatbot = () => {
   const [messages, setMessages] = useState([
-    { id: 1, text: "Hello, how can I help you today?", isBot: 1 },
+    { id: 1, text: "안녕하세요, 무엇을 도와 드릴까요?", isBot: 1 },
   ]);
   const [inputValue, setInputValue] = useState("");
   const { userData } = useData({});
+  const [isloading, setIsloading] = useState(false);
   const navigate = useNavigate();
 
   // 자동 스크롤
@@ -17,13 +26,115 @@ const Chatbot = () => {
     divdiv.scrollTop = divdiv.scrollHeight;
   }, [messages]);
 
-  const handleInputSubmit = (e) => {
+  /*
+  async function send_msg_2_chatgpt(msg) {
+    const response = await openai.createCompletion({
+      model: 'gpt-3.5-turbo',
+      messages : [{'role' : 'user', 'content' : msg}],
+      temperature : 0.0
+    });
+    const data = await response;
+    console.log(data)
+    return data;
+  }*/
+
+  function replaceAll(strTemp, strValue1, strValue2) {
+    while (1) {
+      if (strTemp.indexOf(strValue1) !== -1)
+        strTemp = strTemp.replace(strValue1, strValue2);
+      else break;
+    }
+    return strTemp;
+  }
+
+  async function fetchStream(stream) {
+    const reader = stream.getReader();
+
+    let charsReceived = 0;
+    const li = document.createElement("li");
+
+    // read() returns a promise that resolves
+    // when a value has been received
+    const result = await reader
+      .read()
+      .then(function processText({ done, value }) {
+        // Result objects contain two properties:
+        // done  - true if the stream has already given you all its data.
+        // value - some data. Always undefined when done is true.
+        if (done) {
+          console.log("Stream complete");
+          //console.log(li.innerText);
+          return li.innerText;
+        }
+        // value for fetch streams is a Uint8Array
+        charsReceived += value.length;
+        const decoder = new TextDecoder();
+        const chunk = decoder.decode(value, { stream: true });
+        console.log(
+          `Received ${charsReceived} characters so far. Current chunk = ${chunk}`
+        );
+        li.appendChild(document.createTextNode(chunk));
+        return reader.read().then(processText);
+      });
+    const list = result.split(",");
+    const numList = list.map((item) => {
+      //return unescape(replaceAll(item, "\\", "%"));
+      return item;
+    });
+    //const text = String.fromCharCode(...numList);
+
+    const response = JSON.parse(list);
+    const final_response = response.choices[0].message.content;
+    console.log(final_response);
+    return final_response;
+  }
+
+  //const [chatList, setChatList] = ([]) // chat history
+  async function createCompletion(params = {}, msg) {
+    const DEFAULT_PARAMS = {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: msg }],
+      // max_tokens: 4096,
+      temperature: 0,
+      // frequency_penalty: 1.0,
+      // stream: true,
+    };
+    //console.log(configuration['apiKey'])
+    console.log(msg);
+    const params_ = { ...DEFAULT_PARAMS, ...params };
+    const result = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + my_apikey,
+      },
+      body: JSON.stringify(params_),
+    });
+    const stream = result.body;
+    const output = await fetchStream(stream);
+    console.log(output);
+    return output;
+  }
+
+  const handleInputSubmit = async (e) => {
     e.preventDefault();
     if (inputValue !== "") {
       setMessages([
         ...messages,
         { id: messages.length + 1, text: inputValue, isBot: 0 },
       ]);
+      console.log("This is chatbot.");
+
+      setIsloading(true);
+      const response = await createCompletion({}, inputValue);
+      setIsloading(false);
+      console.log("message:", messages);
+      setMessages([
+        ...messages,
+        { id: messages.length + 1, text: inputValue, isBot: 0 },
+        { id: messages.length + 2, text: response, isBot: 1 },
+      ]);
+
       setInputValue("");
     }
   };
@@ -58,6 +169,7 @@ const Chatbot = () => {
               className="chatbot-message"
               style={{
                 alignSelf: message.isBot ? "flex-start" : "flex-end",
+                backgroundColor: message.isBot ? "greenYellow" : "white",
               }}
             >
               {message.text}
@@ -70,6 +182,7 @@ const Chatbot = () => {
             placeholder="Type your message..."
             value={inputValue}
             onChange={handleInputChange}
+            disabled={isloading}
             className="chatbot-input"
           />
           <button type="submit" className="chatbot-send-button">
