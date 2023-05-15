0778fd794aca3ce0c81a30c8bd7a382c6c3d804e
0112shpark <000112shpark@naver.com>
2023-05-15 14:02:30 +0900

    :bug: fix login button

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./main.css";
+import Nav from "../../components/Nav";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="container">
+      <Nav></Nav>
+      <div className="main-container">소개글 작성해 주세요</div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Nav from "../../components/Nav";
 
 const MyPage = () => {
-  return <div>index</div>;
+  return (
+    <div className="container">
+      <Nav></Nav>
+      <div className="main-container">My page작성해주세요.</div>
+    </div>
+  );
 };
 
 export default MyPage;
