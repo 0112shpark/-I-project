b4e324619ab1e9aaa62e4203ab0e0d1e0f96e01f
0112shpark <000112shpark@naver.com>
2023-05-22 22:02:42 +0900

    :lipstick:handle some error and serach page UI update(05-22)

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,11 +3,17 @@ import Nav from "../../components/Nav";
 import "./main.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import { GrLocation } from "react-icons/gr";
+import { BsFillSunFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FiLoader } from "react-icons/fi";
+import { FaSadTear } from "react-icons/fa";
 
 const SearchPage = () => {
   const [keyword, setKeyword] = useState("");
   const [item, setitem] = useState([]);
+  // const [category, set] = useState("전체");
   const [option, setOption] = useState("");
+  const [isloading, setIsloading] = useState(false);
   const navigate = useNavigate();
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -27,12 +33,14 @@ const SearchPage = () => {
     // 38 => 쇼핑
     // 39 => 음식점
     let URL;
+    setitem([]);
 
     if (!contentid) {
       URL = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=VWVz5AVsiy%2F0nCNOXrxaxJy5b7pzOz3GyOBxO3T8av6rb9xuOhTZpv50%2BbrWeqaaok0Nk77O%2B%2F8wCWW4MPJLNA%3D%3D&listYN=Y&arrange=A&areaCode=&sigunguCode=&cat1=&cat2=&cat3=&keyword=${searchTerm}&_type=json`;
     } else {
       URL = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&contentTypeId=${contentid}&ServiceKey=VWVz5AVsiy%2F0nCNOXrxaxJy5b7pzOz3GyOBxO3T8av6rb9xuOhTZpv50%2BbrWeqaaok0Nk77O%2B%2F8wCWW4MPJLNA%3D%3D&listYN=Y&arrange=A&areaCode=&sigunguCode=&cat1=&cat2=&cat3=&keyword=${searchTerm}&_type=json`;
     }
+    setIsloading(true);
     window
       .fetch(URL)
       .then((res) => res.json())
@@ -44,11 +52,74 @@ const SearchPage = () => {
         const { item } = items;
         setitem(item);
         console.log("item", item);
+        console.log("loading", isloading);
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsloading(false); // Set isLoading to false after data fetching is complete (success or error)
       });
-  }, [contentid, searchTerm]);
+    setIsloading(false);
+  }, [contentid, isloading, searchTerm]);
+
+  const handlecategory = () => {
+    switch (contentid) {
+      case "12":
+        return "관광지";
+
+      case "14":
+        return "문화시설";
+
+      case "15":
+        return "행사/공연/축제";
+
+      case "25":
+        return "여행코스";
+
+      case "28":
+        return "레포츠";
+
+      case "32":
+        return "숙박";
+
+      case "38":
+        return "쇼핑";
+
+      case "39":
+        return "음식점";
+
+      default:
+    }
+    switch (option) {
+      case "12":
+        return "관광지";
+
+      case "14":
+        return "문화시설";
+
+      case "15":
+        return "행사/공연/축제";
+
+      case "25":
+        return "여행코스";
+
+      case "28":
+        return "레포츠";
+
+      case "32":
+        return "숙박";
+
+      case "38":
+        return "쇼핑";
+
+      case "39":
+        return "음식점";
+
+      default:
+    }
+    return "전체";
+  };
 
   const handleinput = (event) => {
     const value = event.target.value;
@@ -71,8 +142,8 @@ const SearchPage = () => {
     <>
       <div className="container">
         <Nav></Nav>
-        <div className="destination">
-          <div className="input flex">
+        <div className="search-destination">
+          <div className="search-input">
             <input
               type="text"
               placeholder="Enter name here..."
@@ -84,7 +155,7 @@ const SearchPage = () => {
           </div>
         </div>
         <div className="option2">
-          <div className="select-wrapper">
+          <div className="search-wrapper">
             <select value={option} onChange={handleOption}>
               <option value="">--Please choose an option--</option>
               <option value="12">관광지</option>
@@ -99,19 +170,47 @@ const SearchPage = () => {
           </div>
         </div>
         <div className="search-topics">
-          "{searchTerm}"에 대한 {option}검색결과입니다.
+          "{searchTerm}"에 대한 {handlecategory()} 검색 결과입니다.
         </div>
         <div className="search-results">
-          {item.map((data) => (
-            <div className="items" key={data.contentid}>
-              {data.firstimage ? (
-                <img src={data.firstimage} alt={data.title} />
-              ) : (
-                <img src="/images/noimage.jpg" alt={data.title} />
-              )}
-              <h2>{data.title}</h2>
+          {isloading ? (
+            <div className="loading">
+              <FiLoader className="icon"></FiLoader>
             </div>
-          ))}
+          ) : item ? (
+            item.map((data) => (
+              <div className="items" key={data.contentid}>
+                {data.firstimage ? (
+                  <img
+                    src={data.firstimage}
+                    className="firstimage"
+                    alt={data.title}
+                  />
+                ) : (
+                  <img
+                    src="/images/noimage.jpg"
+                    className="firstimage"
+                    alt={data.title}
+                  />
+                )}
+                <div className="item-details">
+                  <h2 className="titles">{data.title}</h2>
+                  <div className="item-icons">
+                    <div className="icons">
+                      <AiOutlineHeart className="icon heart"></AiOutlineHeart>
+                      <BsFillSunFill className="icon"></BsFillSunFill>
+                      <BsFillSunFill className="icon"></BsFillSunFill>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="no-results">
+              <img className="sad-tear" src="/images/sad.gif" alt="sad"></img>"
+              {searchTerm}"에 대한 {handlecategory()} 검색 결과가 없습니다.
+            </div>
+          )}
         </div>
       </div>
     </>
